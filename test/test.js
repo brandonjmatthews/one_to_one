036ca77dd524a1b596cde9281fa06c1d58210736
Brandon Matthews <brandon.matthews@jemsoft.co>
2017-03-17 21:37:44 +1030

    Moved core js file for npm compat and updated test.

--- test/test.js
+++ test/test.js
@@ -1,3 +1,11 @@
 var oto = require('../src/js/one_to_one');
-
-oto.ppcm();
+console.log('loaded');
+function resizeRuler() {
+    console.log(oto);
+    var ruler = document.getElementById('ruler');
+    var pixelsPerCM = oto.ppcm();
+    var rulerLength = 30;
+    var pixelLength = 30 * pixelsPerCM.horizontal;
+    console.log(pixelLength);
+    ruler.width = pixelLength;
+};
